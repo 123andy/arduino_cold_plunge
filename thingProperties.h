ca2bfe093fd0c581869f8d75df9b89280980f323
Andy Martin <andy123@stanford.edu>
2025-08-23 11:09:49 -0700

    initial commit

---
+++ thingProperties.h
@@ -0,0 +1,30 @@
+#include <ArduinoIoTCloud.h>
+#include <Arduino_ConnectionHandler.h>
+#include <Arduino_NetworkConfigurator.h>
+#include "configuratorAgents/agents/BLEAgent.h"
+#include "configuratorAgents/agents/SerialAgent.h"
+void onFlowStateChange();
+
+float flowRate;
+int flowState;
+CloudTime cloud_time_read;
+
+KVStore kvStore;
+BLEAgentClass BLEAgent;
+SerialAgentClass SerialAgent;
+WiFiConnectionHandler ArduinoIoTPreferredConnection; 
+NetworkConfiguratorClass NetworkConfigurator(ArduinoIoTPreferredConnection);
+
+void initProperties(){
+  NetworkConfigurator.addAgent(BLEAgent);
+  NetworkConfigurator.addAgent(SerialAgent);
+  NetworkConfigurator.setStorage(kvStore);
+  // For changing the default reset pin uncomment and set your preferred pin. Use DISABLE_PIN for disabling the reset procedure.
+  //NetworkConfigurator.setReconfigurePin(your_pin);
+  ArduinoCloud.setConfigurator(NetworkConfigurator);
+
+  ArduinoCloud.addProperty(flowRate, READ, ON_CHANGE, NULL);
+  ArduinoCloud.addProperty(flowState, READWRITE, ON_CHANGE, onFlowStateChange);
+  ArduinoCloud.addProperty(cloud_time_read, READ, 60 * SECONDS, NULL);
+
+}
